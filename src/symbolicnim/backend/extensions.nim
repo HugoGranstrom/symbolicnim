4bcc8e2cbc74ee9d4dcb1d45b05f44b4b0808bec
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2020-08-23 00:08:12 +0200

    extensions now requires a constructor for functions

--- src/symbolicnim/backend/extensions.nim
+++ src/symbolicnim/backend/extensions.nim
@@ -3,20 +3,27 @@ import ./ast_types
 
 export tables
 
+
+
 var simplifyProcsCT* {.compileTime.} = newSeq[proc(node: SymNode): SymNode]()
 var simplifyProcsRT* = newSeq[proc(node: SymNode): SymNode]()
+
+var constructorProcsCT* {.compileTime.} = initTable[string, proc(inputs: seq[SymNode]): SymNode]()
+var constructorProcsRT* = initTable[string, proc(inputs: seq[SymNode]): SymNode]()
 
 var compileProcsCT* {.compileTime.} = initTable[string, proc(node: SymNode): NimNode]()#{"": proc(node: SymNode): NimNode = discard}.toTable
 
 var diffProcsCT* {.compileTime.} = initTable[string, proc(node, dVar: SymNode): SymNode]()
 var diffProcsRT* = initTable[string, proc(node, dVar: SymNode): SymNode]()
 
-template registerSymFunc*(name: string, diffProc: proc(node, dVar: SymNode): SymNode, compileProc: proc(node: SymNode): NimNode): untyped =
+template registerSymFunc*(name: string, constructorProc: proc(inputs: seq[SymNode]): SymNode, diffProc: proc(node, dVar: SymNode): SymNode, compileProc: proc(node: SymNode): NimNode): untyped =
   static:
-    assert not isValidFunc(name), "Func name '" & name & "' is already used!"
+    assert not isValidFunc(name), "Func name '" & name & "' is already registered!"
     compileProcsCT[name] = compileProc
     diffProcsCT[name] = diffProc
+    constructorProcsCT[name] = constructorProc
   diffProcsRT[name] = diffProc
+  constructorProcsRT[name] = constructorProc
 
 
 proc isValidFunc*(name: string): bool =
